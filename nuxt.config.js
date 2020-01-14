36e5959a9689426a90938cbcfc2242dc5ac4c785
gomezmark <>
2020-01-14 08:21:08 +0800

    change repo name

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ import colors from 'vuetify/es5/util/colors'
 
 const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
   router: {
-    base: '/nuxt-github-page-template/'
+    base: '/portfolio/'
     // base: '/sample-test/'
   }
 } : {};
